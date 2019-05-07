fbb43ad4dc0e46f877ac5a58a43dafa7873cdf1c
yim7 <yimchiu7@gmail.com>
2019-05-07 22:25:52 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-struct Animal(String);
-impl Animal {
-    fn howl(&self) {
-        println!("Fuck!")
-    }
-    fn kill(self) {}
-}
-impl Drop for Animal {
-    fn drop(&mut self) {
-        println!("{} was killed!", self.0)
-    }
-}
-
 fn main() {
     let mut s = String::new();
     for i in 0..4 {
         s.push((i + b'0') as char)
     }
-
-    let tiger = Animal(s.to_string());
-    tiger.howl();
-    tiger.kill();
+    println!("{}", s);
 }
